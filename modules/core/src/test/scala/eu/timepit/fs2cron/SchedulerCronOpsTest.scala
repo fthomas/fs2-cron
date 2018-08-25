06a5dde7235e2349eb51beab0fe1f10d0b0846d7
Frank S. Thomas <frank@timepit.eu>
2018-08-25 23:26:14 +0200

    Test awakeEveryCron

--- modules/core/src/test/scala/eu/timepit/fs2cron/SchedulerCronOpsTest.scala
+++ modules/core/src/test/scala/eu/timepit/fs2cron/SchedulerCronOpsTest.scala
@@ -13,9 +13,14 @@ class SchedulerCronOpsTest extends FunSuite with Matchers {
   val scheduler: Stream[IO, Scheduler] = Scheduler[IO](1)
   val evenSeconds: CronExpr = Cron.unsafeParse("*/2 * * ? * *")
 
+  test("awakeEveryCron") {
+    val s = scheduler.flatMap(_.awakeEveryCron[IO](evenSeconds)) >> evalNow[IO].map(_.getSecond)
+    s.take(2).compile.toList.map(_.forall(_ % 2 == 0)).unsafeRunSync()
+  }
+
   test("sleepCron") {
-    val s = scheduler.flatMap(_.sleepCron[IO](evenSeconds)) >> evalNow[IO]
-    s.compile.last.map(_.forall(_.getSecond % 2 == 0)).unsafeRunSync()
+    val s = scheduler.flatMap(_.sleepCron[IO](evenSeconds)) >> evalNow[IO].map(_.getSecond)
+    s.compile.toList.map(_.forall(_ % 2 == 0)).unsafeRunSync()
   }
 
 }
