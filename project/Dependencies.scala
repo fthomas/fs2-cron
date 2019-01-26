cfe2d3942fdf7d4eff15bb84589f0f3939dd9dce
Scala steward <me@scala-steward.org>
2019-01-26 20:27:46 +0100

    Update fs2-core to 1.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
   val cron4s = "com.github.alonsodomin.cron4s" %% "cron4s-core" % "0.4.5"
-  val fs2Core = "co.fs2" %% "fs2-core" % "1.0.2"
+  val fs2Core = "co.fs2" %% "fs2-core" % "1.0.3"
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
 }
