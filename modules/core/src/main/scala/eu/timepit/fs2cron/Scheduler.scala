f2c090c0bca035e7278043ed498af0b80a75c56d
Frank S. Thomas <frank@timepit.eu>
2021-04-14 07:44:10 +0200

    Inline functions from ScheduledStreams into Scheduler

--- modules/core/src/main/scala/eu/timepit/fs2cron/Scheduler.scala
+++ modules/core/src/main/scala/eu/timepit/fs2cron/Scheduler.scala
@@ -17,6 +17,7 @@
 package eu.timepit.fs2cron
 
 import cats.effect.Temporal
+import fs2.Stream
 
 import scala.concurrent.duration.FiniteDuration
 
@@ -26,4 +27,15 @@ trait Scheduler[F[_], Schedule] {
 
   final def sleepUntilNext(schedule: Schedule): F[Unit] =
     temporal.flatMap(fromNowUntilNext(schedule))(temporal.sleep)
+
+  final def sleep(schedule: Schedule): Stream[F, Unit] =
+    Stream.eval(sleepUntilNext(schedule))
+
+  final def awakeEvery(schedule: Schedule): Stream[F, Unit] =
+    sleep(schedule).repeat
+
+  final def schedule[A](tasks: List[(Schedule, Stream[F, A])]): Stream[F, A] = {
+    val scheduled = tasks.map { case (schedule, task) => awakeEvery(schedule) >> task }
+    Stream.emits(scheduled).covary[F].parJoinUnbounded(temporal)
+  }
 }
