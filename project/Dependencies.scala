687f09ed4fd2185b12eadd85937ca8c7b797c65f
Scala Steward <me@scala-steward.org>
2022-03-12 00:19:08 +0100

    Update calev-core to 0.6.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,5 +4,5 @@ object Dependencies {
   val cron4s = "com.github.alonsodomin.cron4s" %% "cron4s-core" % "0.6.1"
   val fs2Core = "co.fs2" %% "fs2-core" % "3.2.5"
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.11"
-  val calevCore = "com.github.eikek" %% "calev-core" % "0.6.2"
+  val calevCore = "com.github.eikek" %% "calev-core" % "0.6.3"
 }
