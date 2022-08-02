b63842921a75222cc2e3896e4bc4bb304027bd1d
Scala Steward <scala_steward@virtuslab.com>
2022-08-02 14:20:30 +0000

    Update scalatest to 3.2.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,6 @@ import sbt._
 object Dependencies {
   val cron4s = "com.github.alonsodomin.cron4s" %% "cron4s-core" % "0.6.1"
   val fs2Core = "co.fs2" %% "fs2-core" % "3.2.11"
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.12"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.13"
   val calevCore = "com.github.eikek" %% "calev-core" % "0.6.3"
 }
