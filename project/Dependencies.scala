16f14bc27d241cbf9995b3243a61285786fcf6cc
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-02-08 16:43:30 +0100

    Update fs2-core to 3.6.0 (#404)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val cron4s = "com.github.alonsodomin.cron4s" %% "cron4s-core" % "0.6.1"
-  val fs2Core = "co.fs2" %% "fs2-core" % "3.5.0"
+  val fs2Core = "co.fs2" %% "fs2-core" % "3.6.0"
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
   val calevCore = "com.github.eikek" %% "calev-core" % "0.6.4"
 }
